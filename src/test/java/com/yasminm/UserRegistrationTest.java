04b4ea7d0a886cbad5088fbb726c2d0adc5b236e
YasminTrembulack <yasmin.agostinho012@gmail.com>
2024-06-04 13:01:08 -0300

    authentication test

--- src/test/java/com/yasminm/UserRegistrationTest.java
+++ src/test/java/com/yasminm/UserRegistrationTest.java
@@ -5,37 +5,54 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.junit.jupiter.api.Test;
 
 import com.yasminm.model.UserData;
 import com.yasminm.scenes.UserRegistrationSceneController;
 import com.yasminm.util.Authentication;
+import com.yasminm.util.HibernateUtil;
+
+// mvn clean test
 
 public class UserRegistrationTest {
-//  @Test
-//     public void testAuthenticateUser() {
-//         // Teste quando o usuário não existe
-//         Authentication auth = Authentication.authenticateUser("usuario_inexistente", "senha_incorreta");
-//         assertNull(auth.getUser());
-//         assertFalse(auth.getUserExists());
+ @Test
+    public void testAuthenticateUser() {
+         Session session = HibernateUtil
+                .getSessionFactory()
+                .getCurrentSession();
 
-//         // Teste quando o usuário existe, mas a senha está incorreta
-//         auth = Authentication.authenticateUser("usuario_existente", "senha_incorreta");
-//         assertNull(auth.getUser());
-//         assertTrue(auth.getUserExists());
+        Transaction transaction = session.beginTransaction();
 
-//         // Teste quando o usuário existe e a senha está correta
-//         UserData user = new UserData();
-//         user.setUsername("usuario_existente");
-//         user.setPassword("senha_correta");
+        UserData user = new UserData();
+        user.setUsername("usuario_existente");
+        user.setPassword("senha_correta");
+        session.save(user);
+        transaction.commit();
 
-//         // Simulando a existência do usuário no banco de dados
-//         // Você pode substituir isso com mocks ou um banco de dados de teste
-//         // para testes de integração mais realistas
-//         auth.setUser(user);
+        // Teste quando o usuário não existe
+        Authentication auth = Authentication.authenticateUser("usuario_inexistente", "senha_incorreta");
+        assertNull(auth.getUser());
+        assertFalse(auth.getUserExists());
+        
+        // Teste quando o usuário existe, mas a senha está incorreta
+        auth = Authentication.authenticateUser("usuario_existente", "senha_incorreta");
+        assertNull(auth.getUser());
+        assertTrue(auth.getUserExists());
+        
+        // Teste quando o usuário existe e a senha está correta
+        user = new UserData();
+        user.setUsername("usuario_existente");
+        user.setPassword("senha_correta");
+        
+        // Simulando a existência do usuário no banco de dados
+        // Você pode substituir isso com mocks ou um banco de dados de teste
+        // para testes de integração mais realistas
+        auth.setUser(user);
 
-//         auth = Authentication.authenticateUser("usuario_existente", "senha_correta");
-//         assertNotNull(auth.getUser());
-//         assertTrue(auth.getUserExists());
-//     }
+        auth = Authentication.authenticateUser("usuario_existente", "senha_correta");
+        assertNotNull(auth.getUser());
+        assertTrue(auth.getUserExists());
+    }
 }
